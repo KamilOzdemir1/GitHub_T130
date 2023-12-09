9f58332e299d5d4aec322d825bcf8ea72d653e32
Kamil Ozdemir <ozdemirkamil@gmail.com>
2023-12-09 20:08:33 +0100

    2. dosya

---
+++ src/P01.java
@@ -0,0 +1,8 @@
+public class P01 {
+    public static void main(String[] args) {
+
+        int a = 120;
+        int b = 68;
+        System.out.println("iki sayinin toplami : " + (a+b));
+    }
+}
---
+++ src/SecondFiles.java
@@ -0,0 +1,5 @@
+public class SecondFiles {
+    public static void main(String[] args) {
+
+    }
+}
